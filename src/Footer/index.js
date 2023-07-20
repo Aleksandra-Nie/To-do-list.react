4e40a12b9d3feca4ee77e7a496bd323411128b31
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2023-07-20 12:52:36 +0200

    footer format correction

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -2,7 +2,8 @@ import "./style.css"
 
 const Footer = () => (
 
-    <footer className="footer">Copyright © 2023. All right reserved.
+    <footer className="footer">
+        Copyright © 2023. All right reserved.
     </footer>
 );
 
