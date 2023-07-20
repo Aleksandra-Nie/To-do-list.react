7e63c6ec835f1cfccebb2d03622844e1168c8520
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2023-07-20 13:00:00 +0200

    buttons change in className

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -4,15 +4,15 @@ const Buttons = ({ tasks, hideDoneTasks }) => (
     <div className="buttons">
         {tasks.length > 0 && (
             <>
-                <button className="button">
+                <button className="buttons__button">
                     {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
                 </button>
-                <button className="button"
+                <button className="buttons__button"
                     disabled={tasks.every(({ done }) => done)}
                 >
                     Ukończ wszystkie
                 </button>
-                <button type="reset" className="button button--reset">Usuń wszystkie</button>
+                <button type="reset" className="buttons__button buttons__button--reset">Usuń wszystkie</button>
             </>
         )}
     </div>
