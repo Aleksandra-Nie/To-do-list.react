d6792de7b5f44e35fe25fa8a70647d011a36c46e
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2023-11-14 12:12:34 +0100

    GlobalStyle added to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styled.js';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GlobalStyle } from './styled.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
