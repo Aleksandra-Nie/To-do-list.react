5597ee4f85f220fa6c56cd443011385e17fc801a
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2024-01-04 12:35:18 +0100

    dispatch created for toggleHideDone for Buttons component

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,29 +1,35 @@
-import { ButtonField, StyledButton } from "./styled"
+import { selectTasks, toggleHideDone } from "../tasksSlice";
+import { ButtonField, StyledButton } from "./styled";
+import { useSelector, useDispatch } from "react-redux";
 
-const Buttons = ({ tasks, hideDone, toggleHideDone, markAllTasksCompleted, removeAllTasks }) => (
-    <ButtonField>
-        {tasks.length > 0 && (
-            <>
-                <StyledButton
-                    onClick={toggleHideDone}>
-                    {hideDone ? "Pokaż" : "Ukryj"} ukończone
-                </StyledButton>
-                <StyledButton
-                    onClick={markAllTasksCompleted}
-                    disabled={tasks.every(({ done }) => done)}
-                >
-                    Ukończ wszystkie
-                </StyledButton>
-                <StyledButton
-                    $reset
-                    type="reset"
-                    onClick={removeAllTasks}>
-                    Usuń wszystkie
-                </StyledButton>
-            </>
-        )}
-    </ButtonField>
-);
+const Buttons = ({ markAllTasksCompleted, removeAllTasks }) => {
+    const { tasks, hideDone } = useSelector(selectTasks);
+    const dispatch = useDispatch()
 
+    return (
+        <ButtonField>
+            {tasks.length > 0 && (
+                <>
+                    <StyledButton
+                        onClick={() => dispatch(toggleHideDone())}>
+                        {hideDone ? "Pokaż" : "Ukryj"} ukończone
+                    </StyledButton>
+                    <StyledButton
+                        onClick={markAllTasksCompleted}
+                        disabled={tasks.every(({ done }) => done)}
+                    >
+                        Ukończ wszystkie
+                    </StyledButton>
+                    <StyledButton
+                        $reset
+                        type="reset"
+                        onClick={removeAllTasks}>
+                        Usuń wszystkie
+                    </StyledButton>
+                </>
+            )}
+        </ButtonField>
+    );
+}
 
 export default Buttons;
