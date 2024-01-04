4265f150ab3ab0fcb51d138c6408bfd739d98008
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2024-01-04 12:36:50 +0100

    dispatch created for toggleTaskDone for TaskList component

--- src/features/tasks/TaskList/index.js
+++ src/features/tasks/TaskList/index.js
@@ -1,35 +1,41 @@
+import { selectTasks, toggleTaskDone } from "../tasksSlice";
 import { StyledButtonTask, StyledItem, StyledList, StyledContent } from "./styled";
+import { useSelector, useDispatch } from "react-redux";
 
+const TaskList = ({ removeTask }) => {
 
-const TaskList = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
+    const { tasks, hideDone } = useSelector(selectTasks);
+    const dispatch = useDispatch();
 
-    <StyledList>
-        {tasks.map(task => (
-            <StyledItem
-                $hidden={task.done && hideDone}
-                key={task.id}
-            >
-                <StyledButtonTask
-                    $toggleDone
-                    onClick={() => toggleTaskDone(task.id)}
+    return (
+        <StyledList>
+            {tasks.map(task => (
+                <StyledItem
+                    $hidden={task.done && hideDone}
+                    key={task.id}
                 >
-                    {task.done ? " ✓ " : ""}
-                </StyledButtonTask>
-                <StyledContent
-                    $done={task.done}
-                >
-                    {task.content}
-                </StyledContent>
-                <StyledButtonTask
-                    $remove
-                    onClick={() => removeTask(task.id)}
-                >
-                    🗑
-                </StyledButtonTask>
-            </StyledItem>
-        ))}
-    </StyledList >
-);
+                    <StyledButtonTask
+                        $toggleDone
+                        onClick={() => dispatch(toggleTaskDone(task.id))}
+                    >
+                        {task.done ? " ✓ " : ""}
+                    </StyledButtonTask>
+                    <StyledContent
+                        $done={task.done}
+                    >
+                        {task.content}
+                    </StyledContent>
+                    <StyledButtonTask
+                        $remove
+                        onClick={() => removeTask(task.id)}
+                    >
+                        🗑
+                    </StyledButtonTask>
+                </StyledItem>
+            ))}
+        </StyledList >
+    );
+};
 
 
 export default TaskList;
