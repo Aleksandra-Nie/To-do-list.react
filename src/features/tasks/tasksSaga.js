e286c593685434c565653d6e670fc8654894c665
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2024-12-29 17:17:49 +0100

    tasksSaga created

---
+++ src/features/tasks/tasksSaga.js
@@ -0,0 +1,26 @@
+import { call, put, delay, select, takeEvery, takeLatest } from "redux-saga/effects";
+import { fetchExampleTasks, selectTasks, fetchExampleTasksSuccess, fetchExampleTasksError } from "./tasksSlice";
+import { getExampleTasks } from "./getExampleTasks";
+import { saveTasksInLocalStorage } from "./tasksLocalStorage";
+
+function* fetchExampleTasksHandler() {
+    try {
+        yield delay(1000);
+        const exampleTasks = yield call(getExampleTasks);
+        yield put(fetchExampleTasksSuccess(exampleTasks));
+    } catch (error) {
+        yield put(fetchExampleTasksError());
+        yield call(alert, "Coś poszło nie tak!");
+        yield put(fetchExampleTasksSuccess([]));
+    }
+};
+
+function* saveTasksInLocalStorageHandler() {
+    const tasks = yield select(selectTasks);
+    yield call(saveTasksInLocalStorage, tasks);
+};
+
+export function* tasksSaga() {
+    yield takeLatest(fetchExampleTasks.type, fetchExampleTasksHandler);
+    yield takeEvery("*", saveTasksInLocalStorageHandler);
+}; 
