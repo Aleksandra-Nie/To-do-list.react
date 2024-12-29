68d666b3a00f018d945c2f2ae9f008b8d16e7773
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2024-12-29 17:11:43 +0100

    Navigation created

---
+++ src/Navigation/index.js
@@ -0,0 +1,19 @@
+import { toTasks, toAuthor } from "../routes";
+import { Item, List, StyledNavLink } from "./styled";
+
+export default () => {
+
+    return (
+        <nav>
+            <List>
+                <Item>
+                    <StyledNavLink to={toTasks()}>Zadania</StyledNavLink>
+                </Item>
+                <Item>
+                    <StyledNavLink to={toAuthor()}>O autorze</StyledNavLink>
+                </Item>
+            </List>
+        </nav>
+    );
+};
+
