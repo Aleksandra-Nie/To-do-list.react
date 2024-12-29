5388bc6ac52f4cab2262f6df51340324f5b9f4df
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2024-12-29 17:21:46 +0100

    Switch, Route, HashRouter, Redirect added to App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,33 @@
 import React from "react";
-import TasksPage from "./features/tasks/TasksPage/";
+import TasksPage from "./features/tasks/TasksPage";
+import TaskPage from "./features/tasks/TaskPage";
+import AuthorPage from "./features/tasks/author/AuthorPage";
+import Navigation from "./Navigation";
+import {
+    Switch,
+    Route,
+    HashRouter,
+    Redirect
+} from "react-router-dom";
+import { toTasks, toAuthor, toTask } from "./routes";
 
-export default () => <TasksPage />;
+export default () => (
+    <HashRouter>
+        <Navigation />
+
+        <Switch>
+            <Route path={toTask()}>
+                <TaskPage />
+            </Route>
+            <Route path={toTasks()}>
+                <TasksPage />
+            </Route>
+            <Route path={toAuthor()}>
+                <AuthorPage />
+            </Route>
+            <Route>
+                <Redirect to={toTasks()} />
+            </Route>
+        </Switch>
+    </HashRouter>
+);
